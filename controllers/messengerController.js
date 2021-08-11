c429260c711b82a4a20958afd2b8147847f1c050
xuanco941 <68849866+xuanco941@users.noreply.github.com>
2021-08-12 05:35:51 +0700

    none

--- controllers/messengerController.js
+++ controllers/messengerController.js
@@ -1,23 +1,30 @@
 
-const authSchema = require('../model/Schema/authSchema') ;
+const authSchema = require('../model/Schema/authSchema');
+const roomChatSchema = require('../model/Schema/roomChatSchema');
 
-class messengerController{
-    getMessenger(req , res , next) {
+class messengerController {
+    getMessenger(req, res, next) {
         authSchema.findById(req.cookies.userCookie)
-        .then(usermain => {
-            res.render('messenger' , {usermain : usermain.username})
-        }
-    )
-    .catch(next)
+            .then(usermain => {
+                res.render('messenger', { usermain: usermain.username })
+            }
+            )
+            .catch(next)
     }
 
-    getSlugMessenger(req , res , next) {
+    getSlugMessenger(req, res, next) {
+        var userSend = [], userReceive = [];
         authSchema.findById(req.cookies.userCookie)
-        .then(usermain => {
-            res.render('messenger' , {usermain : usermain.username , nameRoom : req.params.room})
-        }
-    )
-    .catch(()=> res.redirect('/vibes/error'));
+            .then(async (usermain) => {
+                await roomChatSchema.findOne({ roomname: req.params.room }).then((room) => {
+                    room.body.forEach(element => {
+                        if(element.user == usermain.username) userSend.push({user : element.user , text : element.text})
+                        else userReceive.push({user : element.user , text : element.text});
+                    });
+                });
+                res.render('messenger', { usermain: usermain.username, nameRoom: req.params.room, userSend, userReceive });
+            })
+            .catch(() => res.redirect('/vibes/error'));
     }
 }
 
